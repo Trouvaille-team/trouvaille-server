f681d54d8dc69f32a997f001b1843472a5273910
MediocreIan <idrews094@gmail.com>
2020-05-29 11:39:44 -0500

    minor changes to work with wiring up

--- src/waypoints/waypointsService.js
+++ src/waypoints/waypointsService.js
@@ -7,36 +7,40 @@ const config = require("../config.js")
 
 const waypointsService = {
   async getPoints(url) {
+    console.log("***************", url)
     let points = []
+    let endCoords = {}
     try {
       const response = await fetch(url);
       const json = await response.json();
       json.routes[0].legs[0].steps.map((step) => {
         points.push(step.end_location);
+        endCoords = json.routes[0].legs[0].end_location
       });
-      return points
+      return { points, endCoords }
     } catch (error) {
       console.log(error);
     }
   },
-  async getWaypoints(arr) {
-
-    let res = []
-    for (let i = 0; i < arr.length; i++) {
-      const element = arr[i];
-      let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${element.lat},${element.lng}&radius=1500&type=tourist_attraction&key=${config.API_KEY}`
+  async getWaypoints(obj) {
+    endCoords = obj.endCoords
+    let points = []
+    for (let i = 0; i < obj.points.length; i++) {
+      const element = obj.points[i];
+      let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${element.lat},${element.lng}&radius=6000&type=tourist_attraction&key=${config.API_KEY}`
+      console.log(url)
       try {
         const response = await fetch(url);
         const json = await response.json();
         json.results.map((place) => {
-          res.push({ name: place.name, id: place.place_id })
+          points.push({ name: place.name, id: place.place_id, coords: place.geometry.location })
         })
       } catch (error) {
         console.log(error);
       }
     }
-    console.log(res)
-    return res
+    console.log(points)
+    return { points, endCoords }
   }
 }
 module.exports = waypointsService
