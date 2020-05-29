378cd51ad32f36e51288cac15952bea44ebd8a88
Douglas Miner <curr3nas@gmail.com>
2020-05-29 10:00:22 -0500

    fixed some issues for production server

--- postgrator-config.js
+++ postgrator-config.js
@@ -8,6 +8,6 @@ module.exports = {
   connectionString:
     process.env.NODE_ENV === 'test'
       ? process.env.TEST_DB_URL
-      : process.env.DB_URL,
+      : process.env.DATABASE_URL,
   ssl: !!process.env.SSL,
 };
--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@
 require("dotenv").config()
 const app = require('./app');
 const knex = require('knex');
-const { PORT, DB_URL } = require('./config');
+const { PORT, DATABASE_URL } = require('./config');
 const db = knex({
   client: 'pg',
-  connection: DB_URL,
+  connection: DATABASE_URL,
 });
 
 app.set('db', db);
