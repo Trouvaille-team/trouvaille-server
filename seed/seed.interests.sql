8faf4d72bbd8dd9145ecaf21b61dc8b783096695
bleek42 <brandonleek42@gmail.com>
2020-06-01 11:11:20 -0400

    interest script: still need keywords for novelty, brew/wine, beach, hiking, mons/lmarks

--- seed/seed.interests.sql
+++ seed/seed.interests.sql
@@ -1,3 +1,14 @@
 INSERT INTO interests (keywords, name)
     VALUES
-    ('campsite campgrounds outdoors', 'camping');
+    ('campsite campgrounds outdoors', 'camping'),
+    ('trail', 'hiking'),
+    ('sand ocean ', 'beaches'),
+    ('', 'breweries/wineries'),
+    ('gallery art', 'museums'),
+    ('', 'novelty'),
+    ('army navy marines', 'military/memorials/battlefields'),
+    ('statues ', 'monuments/landmarks'),
+    ('playgrounds fields', 'parks'),
+    ('mammals reptiles', 'zoos'),
+    ('fair thrill', 'amusement park'),
+    ('ghost spooky scary', 'haunted');
