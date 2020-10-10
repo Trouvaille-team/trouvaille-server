7ecc32b45330d7fb3de8d7d2c1801533324f0a3c
bleek42 <brandonleek42@gmail.com>
2020-10-10 17:39:25 -0400

    emailRouter post route: need to set up trouvaille email account

--- src/email/email-router.js
+++ src/email/email-router.js
@@ -23,4 +23,32 @@ emailRouter.get('/reset', (req, res) => {
   res.render('contact');
 });
 
+emailRouter.post('/confirm', async (req, res, next) => {
+  const { username, password, email} = req.params;
+  try {
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_ACCT,
+        pass: process.env.GMAIL_PW
+      }
+    })
+
+    const mailOptions = {
+      from: process.env.GMAIL_ACCT,
+      to: email,
+      subject: `Trouvaille Email Confirmation - ${username}`,
+      text: 'click the link here to confirm your account with Trouvaille!'
+  };
+
+  await transporter.sendMail(mailOptions, (err, info) => {
+    err ? console.error(err) : console.info(`Success - email sent to ${email}!`)
+  })
+}
+  catch(err) {
+    console.error(err)
+  }
+})
+
 module.exports = emailRouter;
